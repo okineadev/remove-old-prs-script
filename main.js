58a05cc871b4e803675fb96e35f30e5c1b78f287
Okinea Dev <hi@okinea.dev>
2025-01-04 10:27:55 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+import { Octokit } from '@octokit/core';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+(async () => {
+    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+    let page = 1;
+    const per_page = 100;
+
+    let conflictsCount = 0;
+
+    while (true) {
+        const { data } = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+            owner: 'material-extensions',
+            repo: 'vscode-material-icon-theme',
+            state: 'open',
+            per_page,
+            page,
+        });
+
+        if (data.length === 0) break;
+
+        for (const pull of data) {
+            const { data: detailedPull } = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+                owner: 'material-extensions',
+                repo: 'vscode-material-icon-theme',
+                pull_number: pull.number,
+            });
+
+            if (detailedPull.mergeable == false) {
+                conflictsCount++;
+                console.log(`PR #${pull.number}: ${pull.title}`);
+            }
+        }
+        page++;
+    }
+
+    console.log(`Total PRs with conflicts: ${conflictsCount}`);
+})();
